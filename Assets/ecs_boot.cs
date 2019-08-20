5a0fe82209222aa4c95f669171b8f2628ccd9681
abarabone <abara00@live.jp>
2019-08-20 22:01:09 +0900

    system not running

---
+++ Assets/ecs_boot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class ecs_boot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		var w = World.Active;
+		w.CreateSystem<Abss.m.MotionSystem>().Enabled = true;
+
+		var a = w.EntityManager.CreateArchetype( typeof(Abss.Motion.BonePostureData) );
+		var e = w.EntityManager.CreateEntity( a );
+		w.EntityManager.AddComponent<Abss.Motion.BonePostureData>( e );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
