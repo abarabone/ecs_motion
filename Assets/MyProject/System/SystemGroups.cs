fafec986915671e753734b71ffaf4f5d5f4b0a7f
abarabone <abara00@live.jp>
2019-08-24 14:20:05 +0900

    A

---
+++ Assets/MyProject/System/SystemGroups.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.Jobs;
+using Unity.Mathematics;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Rendering;
+using Unity.Properties;
+using Unity.Burst;
+using Abss.Geometry;
+using System.Runtime.InteropServices;
+
+namespace Abss.Motion
+{
+
+
+	[UpdateInGroup(typeof(PresentationSystemGroup))]
+	public class MotionGroup : ComponentSystemGroup { }
+
+
+}
