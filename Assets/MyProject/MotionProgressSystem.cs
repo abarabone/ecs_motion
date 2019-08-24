c5b63bb7ed2576f8c862de3119323d3128a1992c
abarabone <abara00@live.jp>
2019-08-24 12:24:10 +0900

    a

--- Assets/MyProject/MotionProgressSystem.cs
+++ Assets/MyProject/MotionProgressSystem.cs
@@ -20,7 +20,7 @@ namespace Abss.Motion
 {
 	
 
-	
+	[UpdateInGroup(typeof(MotionGroup))]
 	public class MotionProgressSystem : JobComponentSystem
 	{
 		protected override void OnCreate()
@@ -35,62 +35,6 @@ namespace Abss.Motion
 	}
 
 
-	public static class MotionCreator
-	{
-		static public void Create( EntityCommandBuffer ecb )
-		{
-
-			ecb.CreateEntity( archetype, new NativeArray<int>() )
-				var em	= world.GetOrCreateManager<EntityManager>();
-
-				var ent = em.CreateEntity( this.motionArchetype );
-			
-				var ma	= modelData.CreateMotionAccessor( motionIndex );
-
-
-				em.SetComponentData<MotionInfoData>
-				(
-					ent,
-					new MotionInfoData
-					{ 
-						MotionIndex		= 0,
-						DataAccessor	= ma
-					}
-				);
-				//em.SetComponentData<DrawTargetSphere>
-				//(
-				//	ent,
-				//	new DrawTargetSphere
-				//	{
-				//		center	= 0,
-				//		radius	= 1
-				//	}
-				//);
-				//em.SetComponentData<DrawModelInfo>
-				//(
-				//	ent,
-				//	new DrawModelInfo
-				//	{
-				//		modelIndex	= 0
-				//	}
-				//);
-				var buf = em.GetBuffer<MotionStreamElement>( ent );
-				for( var i = 0; i < ma.boneLength * 2 ; ++i )
-				{
-					var stream = new MotionStreamElement();
-
-					stream.Progress.TimeLength	= ma.TimeLength;
-					stream.Progress.TimeScale	= 1.0f;
-					stream.Shift.Keys = ma.GetStreamSlice( i >> 2, KeyStreamSection.positions + (i & 1) ).Keys;
-
-					StreamUtility.InitializeKeys( ref stream.Cache, ref stream.Shift, ref stream.Progress );
-
-					buf.Add( stream );
-				}
-			}
-		
-
-		}
 
 	
 		//static EntityArchetype CreateArchetypes( EntityManager em ) =>
