e89fbb5d2b12318d3a97702bfa43eb89eec654c0
abarabone <abara00@live.jp>
2019-10-24 22:16:42 +0900

    a

--- Assets/MyProject/Scripts/Instance/Arthuring/ColliderAuthoring.cs
+++ Assets/MyProject/Scripts/Instance/Arthuring/ColliderAuthoring.cs
@@ -28,11 +28,34 @@ namespace Abss.Arthuring
 
             var motionClip = this.GetComponent<MotionAuthoring>().MotionClip;//
 
+            var qNameAndBone = motionClip.StreamPaths
+                .Select( x => System.IO.Path.GetFileName( x ) )
+                .Select( ( name, i ) => (name, i: motionClip.IndexMapFbxToMotion[ i ]) )
+                .Where( x => x.i != -1 )
+                .Select( x => (x.name, i: bonePrefabs[ x.i ]) );
+                //.ToDictionary( x => x.name, x => x.i );
 
+            var cs = this.GetComponentsInChildren<Collider>();
+            var qColliders =
+                from collider in cs
+                let parent = collider.gameObject
+                    .AncestorsAndSelf()
+                    .Where( anc => anc.GetComponent<Rigidbody>() != null )
+                    .First()
+                select (collider, parent)
+                ;
+
+            var q =
+                from c in qColliders
+                join b in qNameAndBone
+                on c.collider.name equals b.name
+                select new
+
+                
 
         }
     }
     // 剛体のないコライダは静的として変換する
     // モーションと同名のオブジェクトは、該当するボーンのエンティティにコンポーネントデータを付加する。
-    // 合成コライダはなし
+    // 剛体のついていないコライダは、一番近い先祖剛体に合成
 }
