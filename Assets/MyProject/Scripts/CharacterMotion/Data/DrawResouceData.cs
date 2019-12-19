37270a2fcb4caeb00e08f38d16b49ededbb2b9de
abarabone <abara00@live.jp>
2019-12-19 22:23:05 +0900

    a

---
+++ Assets/MyProject/Scripts/CharacterMotion/Data/DrawResouceData.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Jobs;
+using Unity.Mathematics;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Rendering;
+using Unity.Properties;
+using Unity.Burst;
+using Abss.Geometry;
+using System.Runtime.InteropServices;
+using System;
+
+namespace Abss.Draw
+{
+
+    public struct DrawTotalData : IComponentData
+    {
+        
+    }
+
+    public struct DrawInstanceData : IComponentData
+    {
+        public int VectorLengthInBone;
+        public int BoneLength;
+        //public 
+    }
+
+
+
+    public struct ThreadSafeCounter
+    {
+
+    }
+}
