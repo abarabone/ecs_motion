7c01b6bc1a5cf688b5865d6edf98b5507cbcb652
abarabone <abara00@live.jp>
2020-01-06 22:28:29 +0900

    a

--- Assets/MyProject/PracticeAuthoring.cs
+++ Assets/MyProject/PracticeAuthoring.cs
@@ -25,7 +25,7 @@ namespace Abss.Arthuring
     public class PracticeAuthoring : MonoBehaviour, IConvertGameObjectToEntity
     {
 
-        CharacterAuthoring ch;
+        //public CharacterAuthoring ch;
 
         public void Convert
             ( Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem )
@@ -33,20 +33,20 @@ namespace Abss.Arthuring
 
             //ch.Convert(dstManager, )
 
+            //Debug.Log("aaa");
 
+            var em = dstManager;// World.DefaultGameObjectInjectionWorld.EntityManager;
+            em.AddComponentData( entity, new DrawSystemNativeTransformBufferData { } );
 
-            var em = World.DefaultGameObjectInjectionWorld.EntityManager;
+            //var drawModelArchetype = em.CreateArchetype(
+            //    typeof( DrawModelBoneUnitSizeData ),
+            //    typeof( DrawModelInstanceCounterData ),
+            //    typeof( DrawModelInstanceOffsetData ),
+            //    typeof( DrawModelGeometryData ),
+            //    typeof( DrawModelComputeArgumentsBufferData )
+            //);
 
-
-            var drawModelArchetype = em.CreateArchetype(
-                typeof( DrawModelBoneUnitSizeData ),
-                typeof( DrawModelInstanceCounterData ),
-                typeof( DrawModelInstanceOffsetData ),
-                typeof( DrawModelGeometryData ),
-                typeof( DrawModelComputeArgumentsBufferData )
-            );
-
-            initDrawSystemComponents_( em );
+            //initDrawSystemComponents_( em );
 
             //this.PrefabEntities = this.PrefabGameObjects
             //    //.Select( prefab => prefab.Convert( em, drawMeshCsResourceHolder, initDrawModelComponents_ ) )
@@ -56,83 +56,83 @@ namespace Abss.Arthuring
             return;
 
 
-            void initDrawSystemComponents_( EntityManager em_ )
-            {
-                var ent = entity;
+            //void initDrawSystemComponents_( EntityManager em_ )
+            //{
+            //    var ent = entity;
 
 
-                const int maxBufferLength = 1000 * 16 * 2;//
+            //    const int maxBufferLength = 1000 * 16 * 2;//
 
-                var stride = Marshal.SizeOf( typeof( float4 ) );
+            //    var stride = Marshal.SizeOf( typeof( float4 ) );
 
-                em_.AddComponentData( ent,
-                    new DrawSystemComputeTransformBufferData
-                    {
-                        Transforms = new ComputeBuffer
-                            ( maxBufferLength, stride, ComputeBufferType.Default, ComputeBufferMode.Immutable ),
-                    }
-                );
-                em_.AddComponentData( ent, new DrawSystemNativeTransformBufferData { } );
-            }
+            //    em_.AddComponentData( ent,
+            //        new DrawSystemComputeTransformBufferData
+            //        {
+            //            Transforms = new ComputeBuffer
+            //                ( maxBufferLength, stride, ComputeBufferType.Default, ComputeBufferMode.Immutable ),
+            //        }
+            //    );
+            //    em_.AddComponentData( ent, new DrawSystemNativeTransformBufferData { } );
+            //}
 
-            Entity initDrawModelComponents_( Mesh mesh, Material mat, BoneType boneType )
-            {
-                // キャプチャ（暫定）
-                var em_ = em;
-                var drawModelArchetype_ = drawModelArchetype;
-                var sysEnt_ = entity;
+            //Entity initDrawModelComponents_( Mesh mesh, Material mat, BoneType boneType )
+            //{
+            //    // キャプチャ（暫定）
+            //    var em_ = em;
+            //    var drawModelArchetype_ = drawModelArchetype;
+            //    var sysEnt_ = entity;
 
-                var drawModelEnt = createEntityAndInitComponents_( drawModelArchetype_ );
+            //    var drawModelEnt = createEntityAndInitComponents_( drawModelArchetype_ );
 
-                var boneVectorBuffer = em_.GetComponentData<DrawSystemComputeTransformBufferData>( sysEnt_ ).Transforms;
-                setShaderProps_( mat, mesh, boneVectorBuffer );
+            //    var boneVectorBuffer = em_.GetComponentData<DrawSystemComputeTransformBufferData>( sysEnt_ ).Transforms;
+            //    setShaderProps_( mat, mesh, boneVectorBuffer );
 
-                return drawModelEnt;
+            //    return drawModelEnt;
 
 
-                void setShaderProps_( Material mat_, Mesh mesh_, ComputeBuffer boneVectorBuffer_ )
-                {
-                    mat_.SetBuffer( "BoneVectorBuffer", boneVectorBuffer_ );
-                    mat_.SetInt( "BoneLengthEveryInstance", mesh_.bindposes.Length );
-                    //mat_.SetInt( "BoneVectorOffset", 0 );// 毎フレームのセットが必要
-                }
+            //    void setShaderProps_( Material mat_, Mesh mesh_, ComputeBuffer boneVectorBuffer_ )
+            //    {
+            //        mat_.SetBuffer( "BoneVectorBuffer", boneVectorBuffer_ );
+            //        mat_.SetInt( "BoneLengthEveryInstance", mesh_.bindposes.Length );
+            //        //mat_.SetInt( "BoneVectorOffset", 0 );// 毎フレームのセットが必要
+            //    }
 
-                Entity createEntityAndInitComponents_( EntityArchetype drawModelArchetype__ )
-                {
-                    //var drawModelArchetype = em.CreateArchetype(
-                    //    typeof( DrawModelBoneInfoData ),
-                    //    typeof( DrawModelInstanceCounterData ),
-                    //    typeof( DrawModelInstanceOffsetData ),
-                    //    typeof( DrawModelMeshData ),
-                    //    typeof( DrawModelComputeArgumentsBufferData )
-                    //);
-                    var ent = em.CreateEntity( drawModelArchetype__ );
+            //    Entity createEntityAndInitComponents_( EntityArchetype drawModelArchetype__ )
+            //    {
+            //        //var drawModelArchetype = em.CreateArchetype(
+            //        //    typeof( DrawModelBoneInfoData ),
+            //        //    typeof( DrawModelInstanceCounterData ),
+            //        //    typeof( DrawModelInstanceOffsetData ),
+            //        //    typeof( DrawModelMeshData ),
+            //        //    typeof( DrawModelComputeArgumentsBufferData )
+            //        //);
+            //        var ent = em.CreateEntity( drawModelArchetype__ );
 
-                    em.SetComponentData( ent,
-                        new DrawModelBoneUnitSizeData
-                        {
-                            BoneLength = mesh.bindposes.Length,// より正確なものに変える
-                            VectorLengthInBone = (int)boneType,
-                        }
-                    );
-                    em.SetComponentData( ent,
-                        new DrawModelGeometryData
-                        {
-                            Mesh = mesh,
-                            Material = mat,
-                        }
-                    );
-                    em.SetComponentData( ent,
-                        new DrawModelComputeArgumentsBufferData
-                        {
-                            InstanceArgumentsBuffer = ComputeShaderUtility.CreateIndirectArgumentsBuffer(),
-                        }
-                    );
+            //        em.SetComponentData( ent,
+            //            new DrawModelBoneUnitSizeData
+            //            {
+            //                BoneLength = mesh.bindposes.Length,// より正確なものに変える
+            //                VectorLengthInBone = (int)boneType,
+            //            }
+            //        );
+            //        em.SetComponentData( ent,
+            //            new DrawModelGeometryData
+            //            {
+            //                Mesh = mesh,
+            //                Material = mat,
+            //            }
+            //        );
+            //        em.SetComponentData( ent,
+            //            new DrawModelComputeArgumentsBufferData
+            //            {
+            //                InstanceArgumentsBuffer = ComputeShaderUtility.CreateIndirectArgumentsBuffer(),
+            //            }
+            //        );
 
-                    return ent;
-                }
+            //        return ent;
+            //    }
 
-            }
+            //}
         }
     }
 }
