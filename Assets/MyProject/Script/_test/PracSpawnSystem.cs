6955e69fafbe758d39f124c413047aee33a6f5c2
abarabone <abara00@live.jp>
2020-06-08 23:08:09 +0900

    conversion 練習中

---
+++ Assets/MyProject/Script/_test/PracSpawnSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Collections;
+using Unity.Burst;
+using Unity.Deformations;
+using Unity.Physics;
+using Unity.Jobs;
+
+public class PracSpawnSystem : ComponentSystem
+{
+
+    protected override void OnStartRunning()
+    {
+
+    }
+    
+    protected override void OnUpdate()
+    {
+        this.Entities.ForEach( ( ref wotim  ref SpawnData spawn ) => this.EntityManager.Instantiate( spawn.ent ) );
+    }
+    
+}
---
+++ Assets/MyProject/Script/_test/PractAuthoring.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Collections;
+using Unity.Jobs;
+
+
+public struct SpawnData : IComponentData
+{
+    public Entity ent;
+}
+
+public class PractAuthoring : MonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
+{
+
+    public GameObject prefab;
+
+
+    void IDeclareReferencedPrefabs.DeclareReferencedPrefabs( List<GameObject> referencedPrefabs )
+    {
+
+        referencedPrefabs.Add( this.prefab );
+
+    }
+
+    void IConvertGameObjectToEntity.Convert( Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem )
+    {
+
+        var prefab_ent = conversionSystem.GetPrimaryEntity( this.prefab );
+
+        dstManager.AddComponentData( entity, new SpawnData { ent = prefab_ent } );
+
+    }
+ 
+}
