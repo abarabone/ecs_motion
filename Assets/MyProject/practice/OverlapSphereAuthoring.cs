a596c29f328dc7fa1b9c297d2015141165614fbf
abarabone <abara00@live.jp>
2020-06-27 10:41:04 +0900

    オーバーラップオーサリング追加1

---
+++ Assets/MyProject/practice/OverlapSphereAuthoring.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Collections;
+using Unity.Transforms;
+using Unity.Physics;
+using Unity.Physics.Authoring;
+
+namespace Abarabone.Physics.Authoring
+{
+    public class OverlapSphereAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+    {
+
+
+        public Vector3 Start;
+        public Vector3 Direction;
+        public float Length;
+        public PhysicsCategoryTags BelongsTo;
+        public PhysicsCategoryTags CollidesWith;
+
+
+        public void Convert( Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem )
+        {
+
+        }
+
+    }
+}
