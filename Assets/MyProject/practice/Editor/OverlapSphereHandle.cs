7ec88a1e0dfc20fc3a8dd9b22ea93e9a6fd2c674
abarabone <abara00@live.jp>
2020-06-29 02:06:03 +0900

    overlap sphere authoring エディタハンドルつけた

---
+++ Assets/MyProject/practice/Editor/OverlapSphereHandle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Abarabone.Physics.Authoring;
+
+namespace Abarabone.Model.Authoring
+{
+    [CustomEditor(typeof(OverlapSphereAuthoring))]
+    public class OverlapSphereHandle : Editor
+    {
+        public void OnSceneGUI()
+        {
+            OverlapSphereAuthoring t = this.target as OverlapSphereAuthoring;
+
+            Handles.matrix = Matrix4x4.Scale(Vector3.one * 0.5f);
+
+            EditorGUI.BeginChangeCheck();
+            var center = Handles.PositionHandle(t.Center * 2.0f, Quaternion.identity);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(target, "overlap sphere move center");
+                t.Center = center * 0.5f;
+
+                return;
+            }
+
+            Handles.matrix = Matrix4x4.identity;
+
+            EditorGUI.BeginChangeCheck();
+            float radius = Handles.RadiusHandle(Quaternion.identity, t.Center, t.Radius);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(target, "overlap sphere change radius");
+                t.Radius = radius;
+
+                return;
+            }
+        }
+    }
+}
+
