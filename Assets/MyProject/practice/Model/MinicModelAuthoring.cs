3766e28efc8c57e2956155b7f76ea30d5168dc24
abarabone <abara00@live.jp>
2020-06-30 22:48:25 +0900

    model logic に着手

---
+++ Assets/MyProject/practice/Model/MinicModelAuthoring.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using UnityEngine;
+using Unity.Linq;
+using Unity.Entities;
+using Unity.Collections;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+namespace Abarabone.Model.Authoring
+{
+    using Abarabone.Character;
+    using Abarabone.Draw.Authoring;
+    using Abarabone.Common.Extension;
+
+    /// <summary>
+    /// プライマリエンティティは LinkedEntityGroup のみとする。
+    /// その１つ下に、ＦＢＸのキャラクターを置く。それがメインエンティティとなる。
+    /// </summary>
+    public class MinicModelAuthoring : CharacterModelAuthoring, IConvertGameObjectToEntity
+    {
+
+        public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+        {
+            base.Convert(entity, dstManager, conversionSystem);
+
+
+            dstManager.AddComponentData(entity, new MinicWalkActionState { });
+
+            var post = dstManager.GetComponentData<CharacterLinkData>(entity);//
+            dstManager.AddComponentData(post.PostureEntity, new MoveHandlingData { });//
+            dstManager.AddComponentData(post.PostureEntity, new HorizontalMovingTag { });//
+            dstManager.AddComponentData(post.PostureEntity, new GroundHitResultData { });//
+
+        }
+
+    }
+}
