856d2c1a0ed5bdb737892449ad467309cfe1c76d
abarabone <abara00@live.jp>
2020-10-04 22:59:43 +0900

    mc

---
+++ Assets/MyProject/_/System/BeginInitializationCommandSystemBase.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Burst;
+using Unity.Mathematics;
+using Unity.Collections.LowLevel.Unsafe;
+
+namespace Abarabone.Common
+{
+    [UpdateInGroup(typeof(InitializationSystemGroup))]
+    public class BeginInitializationCommandSystemBase : SystemBase
+    {
+
+
+        protected EntityCommandBufferSystem cmdSystem;
+
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+
+            this.cmdSystem = this.World.GetExistingSystem<BeginInitializationEntityCommandBufferSystem>();
+        }
+
+        protected override void OnUpdate() => OnUpdate();
+    }
+}
