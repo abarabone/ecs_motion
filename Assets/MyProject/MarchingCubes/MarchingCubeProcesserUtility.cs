1e3be7d5f84d340896725f39e574fb7a10bf3f1c
abarabone <abara00@live.jp>
2020-11-04 22:21:25 +0900

    gredhreg

--- Assets/MyProject/MarchingCubes/MarchingCubeProcesserUtility.cs
+++ Assets/MyProject/MarchingCubes/MarchingCubeProcesserUtility.cs
@@ -73,32 +73,32 @@ namespace Abarabone.MarchingCubes
         static public void makeAlterX_(ref UnsafeList<uint4> cubeBitwises)
         {
             var i = 0;
-            for (var iy = 0; iy < 16; iy++)
-            {
-                for (var iz = 0; iz < 4; iz++)
-                {
-                    var _98109810 = cubeBitwises[i++];
-                    var _ba32ba32 = cubeBitwises[i++];
-                    var _dc54dc54 = cubeBitwises[i++];
-                    var _fe76fe76 = cubeBitwises[i++];
+            //for (var iy = 0; iy < 16; iy++)
+            //{
+            //    for (var iz = 0; iz < 4; iz++)
+            //    {
+            //        var _98109810 = cubeBitwises[i++];
+            //        var _ba32ba32 = cubeBitwises[i++];
+            //        var _dc54dc54 = cubeBitwises[i++];
+            //        var _fe76fe76 = cubeBitwises[i++];
 
-                    var m55 = 0x_5555_5555u;
-                    var maa = 0x_aaaa_aaaau;
-                    var _a921a921 = (_ba32ba32 & m55) << 1 | (_98109810 & maa) >> 1;
-                    var _cb43cb43 = (_dc54dc54 & m55) << 1 | (_ba32ba32 & maa) >> 1;
-                    var _ed65ed65 = (_fe76fe76 & m55) << 1 | (_dc54dc54 & maa) >> 1;
-                    var __f870f87 = (_98109810 >> 8 & 0x_55_5555u) << 1 | (_fe76fe76 & maa) >> 1;
-                    // a9a9a9a921212121a9a9a9a921212121
-                    // cbcbcbcb43434343cbcbcbcb43434343
-                    // edededed65656565edededed65656565
-                    // -f-f-f-f878787870f0f0f0f87878787
+            //        var m55 = 0x_5555_5555u;
+            //        var maa = 0x_aaaa_aaaau;
+            //        var _a921a921 = (_ba32ba32 & m55) << 1 | (_98109810 & maa) >> 1;
+            //        var _cb43cb43 = (_dc54dc54 & m55) << 1 | (_ba32ba32 & maa) >> 1;
+            //        var _ed65ed65 = (_fe76fe76 & m55) << 1 | (_dc54dc54 & maa) >> 1;
+            //        var __f870f87 = (_98109810 >> 8 & 0x_55_5555u) << 1 | (_fe76fe76 & maa) >> 1;
+            //        // a9a9a9a921212121a9a9a9a921212121
+            //        // cbcbcbcb43434343cbcbcbcb43434343
+            //        // edededed65656565edededed65656565
+            //        // -f-f-f-f878787870f0f0f0f87878787
 
-                    var y0z1 = ;
-                    var y0z2 = ;
-                    var y1z1 = ;
-                    var y1z2 = ;
-                }
-            }
+            //        var y0z1 = ;
+            //        var y0z2 = ;
+            //        var y1z1 = ;
+            //        var y1z2 = ;
+            //    }
+            //}
         }
 
         [StructLayout(LayoutKind.Sequential)]
