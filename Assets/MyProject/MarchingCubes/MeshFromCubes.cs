d6500e3ad8f69e548c063b78f621bd0d541983ca
abarabone <abara00@live.jp>
2020-11-13 21:46:45 +0900

    update packages

--- Assets/MyProject/MarchingCubes/MeshFromCubes.cs
+++ Assets/MyProject/MarchingCubes/MeshFromCubes.cs
@@ -8,7 +8,7 @@ using Unity.Collections;
 using Unity.Jobs;
 using Unity.Burst;
 using Unity.Collections.LowLevel.Unsafe;
-using Unity.Collections.Experimental;
+//using Unity.Collections.Experimental;
 
 namespace Abarabone.MarchingCubes
 {
