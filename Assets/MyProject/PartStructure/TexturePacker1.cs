aa9478d31ab6b992fec950afc389850c151b221d
abarabone <abara00@live.jp>
2020-12-15 01:25:21 +0900

    texture packer dict rewrited

--- Assets/MyProject/PartStructure/TexturePacker1.cs
+++ Assets/MyProject/PartStructure/TexturePacker1.cs
@@ -9,50 +9,28 @@ namespace Abarabone.Geometry
 	using Abarabone.Common.Extension;
 
 
-	public static class TexturePacker1
+	public static class TexturePacker
 	{
 
 
-		public static (Texture atlas, Dictionary<Texture2D, Mesh> newmeshes) Pack(IEnumerable<GameObject> targetObjects)
+		public static (Texture2D atlas, Dictionary<Mesh, Mesh> newmeshes) Pack(IEnumerable<GameObject> targetObjects)
         {
 
 			var mmts = FromObject.QueryMeshMatsTransform_IfHaving(targetObjects).ToArray();
+
 
 			var uniqueTextures = queryUniqueTextures_(mmts.Select(x => x.mats));
 
 			var (atlas, uvOffsets) = packTexture_(uniqueTextures);
 
-			var uvOffsetDict = (uniqueTextures, uvOffsets).Zip()
-				.ToDictionary(x => x.x, x => x.y);
-			uvOffsetDict.Add(null, new Rect(0,0,1,1));
 
+			var uvOffsetDict = makeUvOffsetDict_(uniqueTextures, uvOffsets);
 
-			var qSubmeshUv =
-				from mmt in mmts
-				select mmt.mesh.SubmeshVertices(mmt.mesh.uv)
-				;
-			var qSubmeshTex =
-				from mmt in mmts
-				select
-					from mat in mmt.mats
-					select mat.mainTexture as Texture2D
-				;
+			var qMeshNewUv = queryMeshNewUv_(mmts, uvOffsetDict);
 
-			var qSubmeshUvNew =
-				from obj in (qSubmeshUv, qSubmeshTex).Zip()
-				select
-					from x in (obj.x, obj.y).Zip()
-					let uvs = x.x
-					let tex = x.y
-					select
-						from uv in uvs
-						select uv.ScaleUv(uvOffsetDict[tex])
-				;
+			var meshPairDict = makeMeshPairDict_(mmts, qMeshNewUv);
 
-			var qMesh = (mmts, qSubmeshUvNew).Zip()
-				.Select(x => (src: x.x.mesh, dst: cloneMesh_(x.x.mesh, x.y.SelectMany().ToArray());
-
-			return (atlas, newmeshes);
+			return (atlas, meshPairDict);
 
 
 			static Texture2D[] queryUniqueTextures_(IEnumerable<Material[]> matss) =>
@@ -64,7 +42,7 @@ namespace Abarabone.Geometry
 					.ToArray();
 
 
-			static (Texture, Rect[]) packTexture_(IEnumerable<Texture2D> srcTextures)
+			static (Texture2D, Rect[]) packTexture_(IEnumerable<Texture2D> srcTextures)
 			{
 				//var dstTexture = new Texture2D( 0, 0 );
 				var dstTexture = new Texture2D
@@ -76,18 +54,72 @@ namespace Abarabone.Geometry
 				return (dstTexture, uvRects);
 			}
 
-			static Mesh cloneMesh_(Mesh srcmesh, Vector2[] newuv)
-            {
-				var go = new GameObject("dummy for mesh copy");
-				var mf = go.AddComponent<MeshFilter>();
-				mf.mesh = srcmesh;
-				GameObject.Destroy(go);
+			static Dictionary<Texture2D, Rect> makeUvOffsetDict_
+				(Texture2D[] uniqueTextures, Rect[] uvOffsets)
+			{
+				var uvOffsetDict = (uniqueTextures, uvOffsets).Zip()
+					.ToDictionary(x => x.x, x => x.y);
 
-				var newmesh = mf.mesh;
-				newmesh.uv = newuv;
+				uvOffsetDict.Add(null, new Rect(0, 0, 1, 1));
 
-				return newmesh;
-            }
+				return uvOffsetDict;
+			}
+
+			static IEnumerable<IEnumerable<Vector2>> queryMeshNewUv_
+				(IEnumerable<(Mesh mesh, Material[] mats, Transform tf)> mmts, Dictionary<Texture2D, Rect> uvOffsetDict)
+			{
+				var qSubmeshUv =
+					from mmt in mmts
+					select mmt.mesh.SubmeshVertices(mmt.mesh.uv)
+					// 頂点は若い submesh の順に、隙間なく格納されていると仮定する
+					;
+				var qSubmeshTex =
+					from mmt in mmts
+					select
+						from mat in mmt.mats
+						select mat.mainTexture as Texture2D
+					;
+
+				var qMeshUvNew =
+					from obj in (qSubmeshUv, qSubmeshTex).Zip()
+					select
+						from x in (obj.x, obj.y).Zip()
+						let uvs = x.x
+						let tex = x.y
+						from uv in uvs
+						select uv.ScaleUv(uvOffsetDict[tex])
+					;
+
+				return qMeshUvNew;
+			}
+
+			static Dictionary<Mesh, Mesh> makeMeshPairDict_
+				(IEnumerable<(Mesh mesh, Material[] mats, Transform tf)> mmts, IEnumerable<IEnumerable<Vector2>> meshUvNew)
+			{
+				var qMeshPair =
+					from x in (mmts, meshUvNew).Zip()
+					let src = x.x.mesh
+					let dst = cloneMesh_(x.x.mesh, x.y.ToArray())
+					select (src, dst)
+					;
+
+				return qMeshPair.ToDictionary(x => x.src, x => x.dst);
+
+
+				static Mesh cloneMesh_(Mesh srcmesh, Vector2[] newuv)
+				{
+					var go = new GameObject("dummy for mesh copy");
+					var mf = go.AddComponent<MeshFilter>();
+					mf.mesh = srcmesh;
+					GameObject.Destroy(go);
+
+					var newmesh = mf.mesh;
+					newmesh.uv = newuv;
+
+					return newmesh;
+				}
+			}
+
 		}
 
 	}
