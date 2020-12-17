5a2d9b4a4b40e07382ea66f3f1cc3131797c7d10
abarabone <abara00@live.jp>
2020-12-17 23:46:38 +0900

    st

---
+++ Assets/MyProject/PartStructure/Authoring/Area/StructureAreaAuthoring.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Collections;
+using Unity.Transforms;
+using Unity.Mathematics;
+using System.Threading.Tasks;
+using Unity.Linq;
+using UnityEditor;
+
+namespace Abarabone.Structure.Authoring2
+{
+
+    using Abarabone.Model;
+    using Abarabone.Draw;
+    using Abarabone.Model.Authoring;
+    using Abarabone.Draw.Authoring;
+    using Abarabone.Geometry;
+    using Unity.Physics.Authoring;
+    using System.Runtime.InteropServices;
+    using Abarabone.Common.Extension;
+    using Abarabone.Structure.Authoring;
+
+    public class StructureAreaAuthoring : MonoBehaviour, IDeclareReferencedPrefabs, IConvertGameObjectToEntity
+    {
+
+        //public bool IsCombineMesh = true;
+        //public bool IsPackTexture = true;
+
+
+        public StructureModelAuthoring[] StructureModelPrefabs;
+
+        (GameObject, Mesh)[] objectsAndMeshes;
+        //GameObject[] partMasterPrefabs;
+
+
+        public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
+        {
+            createMeshes();
+            var structurePrefabs = this.StructureModelPrefabs.Select( x => x.gameObject );
+            referencedPrefabs.AddRange(structurePrefabs);
+        }
+
+        public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+        {
+
+            dstManager.DestroyEntity(entity);
+
+            addToMeshDictionary_(this.objectsAndMeshes);
+
+            //stantiateMasterPrefab_ForConversion_(conversionSystem, this.partMasterPrefabs);
+
+            return;
+
+
+            void addToMeshDictionary_((GameObject, Mesh)[] objectsAndMeshes_)
+            {
+                foreach (var (go, mesh) in objectsAndMeshes_)
+                {
+                    Debug.Log($"to dict {go.name} - {mesh.name}");
+                    conversionSystem.AddToMeshDictionary(go, mesh);
+                }
+            }
+        }
+
+        void createMeshes()
+        {
+
+            var qNear =
+                from st in this.StructureModelPrefabs.Do(x => Debug.Log(x.NearMeshObject.objectTop.name))
+                select st.GetNearMeshFunc()
+                ;
+            var qFar =
+                from st in this.StructureModelPrefabs.Do(x => Debug.Log(x.FarMeshObject.objectTop.name))
+                select st.GetFarMeshAndFunc()
+                ;
+            var qPartAll =
+                from st in this.StructureModelPrefabs
+                from pt in st.GetComponentsInChildren<StructurePartAuthoring>()
+                select pt
+                ;
+            var qPartDistinct =
+                from pt in qPartAll.Distinct(pt => pt.MasterPrefab)
+                select pt.GetPartsMeshesAndFuncs()
+                ;
+
+
+            var allMeshFuncs = qNear.Concat(qFar).Concat(qPartDistinct).ToArray();
+
+            var qGoAndTask = allMeshFuncs
+                .Where(x => x.f != null)
+                .Select(x => (x.go, t: Task.Run(x.f)));
+
+            var qGoAndMesh = allMeshFuncs
+                .Where(x => x.mesh != null)
+                .Select(x => (x.go, x.mesh));
+
+            var qGoAndMeshFromTask = qGoAndTask
+                .Select(x => x.t)
+                .WhenAll()
+                .Result
+                .Select(x => x.CreateMesh())
+                .Zip(qGoAndTask, (x, y) => (y.go, mesh: x));
+
+
+            this.objectsAndMeshes = qGoAndMesh.Concat(qGoAndMeshFromTask)
+                .ToArray();
+        }
+
+
+
+
+    }
+
+    static class StructureConversionExtension
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        static public Task<T[]> WhenAll<T>(this IEnumerable<Task<T>> tasks) =>
+            Task.WhenAll(tasks);
+
+    }
+
+}
