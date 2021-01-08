9a579b0e1ad3dc35e5b25f08eea80f3e94059ee2
abarabone <abara00@live.jp>
2021-01-09 00:54:30 +0900

    mesh util

--- Assets/DotsLightWeight/Utility/new_mesh_util/MeshUtility.cs
+++ Assets/DotsLightWeight/Utility/new_mesh_util/MeshUtility.cs
@@ -1,16 +1,16 @@
-using System.Collections;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
-using Unity.Linq;
-using System.Threading.Tasks;
-using System;
-using Abarabone.Common.Extension;
 using UnityEngine.Rendering;
-using System.Runtime.CompilerServices;
+using Unity.Collections;
+using Unity.Linq;
 
 namespace Abarabone.Geometry
 {
+    using Abarabone.Common.Extension;
     using Abarabone.Utilities;
 
     static public class MeshInWorkerThreadUtility
@@ -27,88 +27,119 @@ namespace Abarabone.Geometry
 
         public struct SubMesh<T> where T : struct
         {
-            public SubMesh(int i, IEnumerable<T> submeshVertexData)
+            public SubMesh(int i, IEnumerable<T> submeshElements)
             {
-                this.VertexData = submeshVertexData;
                 this.SubMeshIndex = i;
+                this.Elements = submeshElements;
             }
 
             public int SubMeshIndex { get; private set; }
-            public IEnumerable<T> VertexData { get; private set; }
+            public IEnumerable<T> Elements { get; private set; }
         }
 
-        static public IEnumerable<SubMesh<T>> VertexSubMeshes<T>(this Mesh.MeshData meshdata)
-            where T:struct
-        {
-            var vtxs = meshdata.GetVertexData<T>();
 
-            var i = 0;
-            foreach (var range in meshdata.getSubmeshVertexRanges_())
-            {
-                yield return new SubMesh<T>(i++, getVerticesInSubmesh_());
-
-                IEnumerable<T> getVerticesInSubmesh_()
-                {
-                    for (var i = range.start; i < range.length; i++)
-                    {
-                        yield return vtxs[i];
-                    }
-                }
-            }
-		}
-
-		static IEnumerable<(int start, int length)> getSubmeshVertexRanges_(this Mesh.MeshData meshdata) =>
-            from i in 0.UpTo(meshdata.subMeshCount)
-            let submeshDescriptor = meshdata.GetSubMesh(i)
-            select (fst: submeshDescriptor.firstVertex, length: submeshDescriptor.vertexCount)
-            ;
-
-
-        static public IEnumerable<SubMesh<T>> IndexSubMeshes<T>(this Mesh.MeshData meshdata)
+        static public IEnumerable<IEnumerable<T>> AsTriangle<T>(this IEnumerable<T> indecies_)
             where T : struct
         {
-            var idxs = meshdata.GetIndexData<T>();
+            using var e = indecies_.GetEnumerator();
 
-            var i = 0;
-            foreach (var range in meshdata.getSubmeshIndexRanges_())
+            while (e.MoveNext())
             {
-                yield return new SubMesh<T>(i++, getIndicesInSubmesh_());
+                yield return tri_();
 
-                IEnumerable<T> getIndicesInSubmesh_()
+                IEnumerable<T> tri_()
                 {
-                    for (var i = range.start; i < range.length; i++)
+                    yield return e.Current; e.MoveNext();
+                    //if (!e.MoveNext()) yield break;
+                    yield return e.Current; e.MoveNext();
+                    //if (!e.MoveNext()) yield break;
+                    yield return e.Current;
+                }
+            }
+        }
+
+
+        static public IEnumerable<SubMesh<T>> VertexInSubMeshes<T>(this Mesh.MeshData meshdata) where T : struct =>
+            meshdata.ElementsInSubMesh(meshdata.GetVertexData<T>(), (submesh, vtx) => vtx);
+
+        static public IEnumerable<SubMesh<T>> IndexInSubMeshes<T>(this Mesh.MeshData meshdata) where T : struct =>
+            meshdata.ElementsInSubMesh(meshdata.GetIndexData<T>(), (submesh, idx) => idx + submesh.baseVertex);
+
+
+        //static public IEnumerable<SubMesh<int>> IndicesInSubMesh2(this Mesh.MeshData meshdata, NativeArray<int> srcArray)
+        //{
+        //    var i = 0;
+        //    foreach (var desc in meshdata.submeshDescripters_())
+        //    {
+        //        yield return new SubMesh<int>(i++, getElementsInSubmesh_());
+
+        //        IEnumerable<int> getElementsInSubmesh_()
+        //        {
+        //            for (var i = desc.indexStart; i < desc.indexCount; i++)
+        //            {
+        //                yield return srcArray[i] + desc.baseVertex;
+        //            }
+        //        }
+        //    }
+        //}
+
+        static public IEnumerable<SubMesh<T>> ElementsInSubMesh<T>
+            (this Mesh.MeshData meshdata, NativeArray<T> srcArray, Func<SubMeshDescriptor,T,T> conversion) where T : struct
+        {
+            var i = 0;
+            foreach (var desc in meshdata.submeshDescripters_())
+            {
+                yield return new SubMesh<T>(i++, getElementsInSubmesh_());
+
+                IEnumerable<T> getElementsInSubmesh_()
+                {
+                    for (var i = desc.firstVertex; i < desc.vertexCount; i++)
                     {
-                        yield return idxs[i];
+                        yield return conversion(desc, srcArray[i]);
                     }
                 }
             }
         }
 
-        static IEnumerable<(int start, int length)> getSubmeshIndexRanges_(this Mesh.MeshData meshdata) =>
+        static IEnumerable<SubMeshDescriptor> submeshDescripters_(this Mesh.MeshData meshdata) =>
             from i in 0.UpTo(meshdata.subMeshCount)
-            let submeshDescriptor = meshdata.GetSubMesh(i)
-            select (fst: submeshDescriptor.indexStart, length: submeshDescriptor.indexCount)
+            select meshdata.GetSubMesh(i)
             ;
 
-        static IEnumerable<IEnumerable<int>> triangle_(IEnumerable<int> indecies_)
-        {
-            using (var e = indecies_.GetEnumerator())
-            {
-                for (; ; )
-                {
-                    yield return tri_();
+        //static IEnumerable<(int start, int length)> getSubmeshVertexRanges_(this Mesh.MeshData meshdata) =>
+        //    from i in 0.UpTo(meshdata.subMeshCount)
+        //    let submeshDescriptor = meshdata.GetSubMesh(i)
+        //    select (start: submeshDescriptor.firstVertex, length: submeshDescriptor.vertexCount)
+        //    ;
 
-                    IEnumerable<int> tri_()
-                    {
-                        yield return e.Current;
-                        if (!e.MoveNext()) yield break;
-                        yield return e.Current;
-                        if (!e.MoveNext()) yield break;
-                        yield return e.Current;
-                        if (!e.MoveNext()) yield break;
-                    }
-                }
-            }
+        //static IEnumerable<(int start, int length)> getSubmeshIndexRanges_(this Mesh.MeshData meshdata) =>
+        //    from i in 0.UpTo(meshdata.subMeshCount)
+        //    let submeshDescriptor = meshdata.GetSubMesh(i)
+        //    select (start: submeshDescriptor.indexStart, length: submeshDescriptor.indexCount)
+        //    ;
+
+
+        static void aaa(IEnumerable<GameObject> gameObjects, Transform tfBase)
+        {
+            var mmts = FromObject.QueryMeshMatsTransform_IfHaving(gameObjects).ToArray();
+
+            var meshes = mmts.Select(x => x.mesh).ToArray();
+
+            var srcmeshes = Mesh.AcquireReadOnlyMeshData(meshes);
+            var dstmesh = Mesh.AllocateWritableMeshData(1)[0];
+
+            var qTri =
+                from mesh in srcmeshes.AsEnumerable()
+                from submesh in mesh.IndexInSubMeshes<int>()
+                from tri in submesh.Elements.AsTriangle()
+                select tri
+                ;
+            var qVtx =
+                from mesh in srcmeshes.AsEnumerable()
+                from submesh in mesh.VertexInSubMeshes<Vector3>()
+                from vtx in submesh.Elements
+                select vtx
+                ;
         }
     }
 
