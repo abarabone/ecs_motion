63deae428c2ee6ffce1b74c910c97746465852ed
abarabone <abara00@live.jp>
2021-02-11 22:31:05 +0900

    a

--- Assets/DotsLightWeight/Utility/new_mesh_util/MeshCombineUtility.cs
+++ Assets/DotsLightWeight/Utility/new_mesh_util/MeshCombineUtility.cs
@@ -81,6 +81,96 @@ namespace Abarabone.Geometry
 
 
 
+
+        static IEnumerable<IEnumerable<MeshUnit>> queryMeshData
+            (this IEnumerable<IEnumerable<(Mesh mesh, Material[] mats, Transform tf)>> mmtss)
+        {
+            var meshes = mmtss.SelectMany().Select(x => x.mesh).ToArray();
+            var mesharr = Mesh.AcquireReadOnlyMeshData(meshes);
+
+            var imesh = 0;
+
+            return
+                from mmts in mmtss
+                let length = mmts.Count()
+                select queryMesh_(imesh, length)
+                ;
+
+            IEnumerable<MeshUnit> queryMesh_(int first, int length)
+            {
+                var baseVertex = 0;
+                for (var i = first; i < first + length; i++)
+                {
+                    yield return new MeshUnit(i, mesharr[i], baseVertex);
+
+                    baseVertex += mesharr[i].vertexCount;
+                }
+            }
+        }
+        static IEnumerable<Mesh.MeshData> asEnumerable(this Mesh.MeshDataArray meshDataArray)
+        {
+            for (var i = 0; i < meshDataArray.Length; i++)
+            {
+                yield return meshDataArray[i];
+            }
+        }
+
+
+
+        static AdditionalParameters calculateParametors
+            (
+                this IEnumerable<(Mesh mesh, Material[] mats, Transform tf)> mmts,
+                Transform tfBase, Func<int, Rect> texHashToUvRectFunc,
+                Transform[] tfBones
+            )
+        {
+            var mmts_ = mmts.ToArray();
+            var meshes = mmts_
+                .Select(x => x.mesh)
+                .ToArray();
+
+            var qMtPerMesh = mmts_
+                .Select(x => x.tf.localToWorldMatrix);
+            var qTexhashPerSubMesh =
+                from mmt in mmts_
+                select
+                    from mat in mmt.mats
+                    select mat.mainTexture?.GetHashCode() ?? default
+                ;
+
+            var mtBaseInv = tfBase.worldToLocalMatrix;
+
+
+            var result = new AdditionalParameters
+            {
+                mtBaseInv = mtBaseInv,
+                mtPerMesh = qMtPerMesh.ToArray(),
+                texhashPerSubMesh = qTexhashPerSubMesh.ToArrayRecursive2(),
+                //atlasHash = atlas?.GetHashCode() ?? 0,
+                //texhashToUvRect = texHashToUvRect,
+                texHashToUvRect = texHashToUvRectFunc,
+            };
+            if (tfBones == null) return result;
+
+
+            var qBoneWeights =
+                from mesh in meshes
+                select mesh.boneWeights
+                ;
+            var qMtInvs =
+                from mesh in meshes
+                select mesh.bindposes
+                ;
+            var qSrcBones = mmts_
+                .Select(x => x.tf.GetComponentOrNull<SkinnedMeshRenderer>()?.bones ?? x.tf.AsEnumerable().ToArray());
+            ;
+            result.boneWeightsPerMesh = qBoneWeights.ToArray();
+            result.mtInvsPerMesh = qMtInvs.ToArray();
+            result.srcBoneIndexToDstBoneIndex = (qSrcBones, tfBones).ToBoneIndexConversionDictionary();
+            return result;
+        }
+
+
         static (Mesh.MeshDataArray, AdditionalParameters) calculateParametors
             (
                 this IEnumerable<(Mesh mesh, Material[] mats, Transform tf)> mmts,
