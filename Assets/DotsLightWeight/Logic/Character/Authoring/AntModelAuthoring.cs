f82761f35b652a5f7af14f06db67fb7e05d6c48e
abarabone <abara00@live.jp>
2021-03-23 02:52:26 +0900

    ant 着手

---
+++ Assets/DotsLightWeight/Logic/Character/Authoring/AntModelAuthoring.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using UnityEngine;
+using Unity.Linq;
+using Unity.Entities;
+using Unity.Collections;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+namespace Abarabone.Model.Authoring
+{
+    using Abarabone.Character;
+    using Abarabone.Draw.Authoring;
+    using Abarabone.Common.Extension;
+    using Abarabone.Draw;
+    using Abarabone.CharacterMotion;
+    using Abarabone.Arms;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AntModelAuthoring : CharacterModelAuthoring, IConvertGameObjectToEntity
+    {
+
+
+        public new void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+        {
+            base.Convert(entity, dstManager, conversionSystem);
+
+
+            var top = this.gameObject;
+            var main = top.transform.GetChild(0).gameObject;
+
+            initMain_(conversionSystem, main);
+
+            return;
+
+
+            void initMain_( GameObjectConversionSystem gcs_, GameObject main_ )
+            {
+                var em = gcs_.DstEntityManager;
+
+                var mainEntity = gcs_.GetPrimaryEntity(main_);
+
+                var types = new ComponentTypes
+                (
+                    //typeof(PlayerTag),
+
+                    //typeof(HorizontalMovingTag),
+                    //typeof(MoveHandlingData),
+                    typeof(GroundHitResultData)
+
+                    //typeof(MinicWalkActionState)
+                );
+                em.AddComponents(mainEntity, types);
+            }
+
+
+        }
+
+    }
+}
