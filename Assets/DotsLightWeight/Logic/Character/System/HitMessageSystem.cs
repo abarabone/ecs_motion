b34308eb939758f3850409dde1c86a3dda74906b
abarabone <abara00@live.jp>
2021-04-03 00:17:15 +0900

    a

--- Assets/DotsLightWeight/Logic/Character/System/HitMessageSystem.cs
+++ Assets/DotsLightWeight/Logic/Character/System/HitMessageSystem.cs
@@ -3,32 +3,92 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Entities;
 using Unity.Collections;
+using Unity.Jobs;
 
 namespace Abarabone.Character.Action
 {
 
-    //public struct HitMessageUnit
-    //{
-    //    public Entity mainFrom;
-    //    public float damage;
-    //}
+    public struct HitMessageUnit
+    {
+        public float damage;
+    }
 
-    //public struct HitLinker
-    //{
-    //    public Entity 
-    //}
-
-    //public class HitMessageSystem : SystemBase
-    //{
-
-    //    NativeQueue<HitMessageUnit> queue;
-
-    //    NativeList<HitMessageUnit> 
+    [UpdateInGroup(typeof(SystemGroup.Presentation.Logic.ObjectLogicSystemGroup))]
+    public class HitMessageSystem : SystemBase
+    {
 
 
-    //    protected override void OnUpdate()
-    //    {
 
-    //    }
-    //}
+        NativeList<Entity> keyEntities;
+        NativeMultiHashMap<Entity, HitMessageUnit> messageHolder;
+        ParallelWriter writer;
+
+        public struct ParallelWriter
+        {
+            NativeList<Entity>.ParallelWriter nl;
+            NativeMultiHashMap<Entity, HitMessageUnit>.ParallelWriter hm;
+            public ParallelWriter(ref NativeList<Entity> nl, ref NativeMultiHashMap<Entity, HitMessageUnit> hm)
+            {
+                this.nl = nl.AsParallelWriter();
+                this.hm = hm.AsParallelWriter();
+            }
+            public void Add(Entity entity, HitMessageUnit hitMessage)
+            {
+                this.nl.AddNoResize(entity);
+                this.hm.Add(entity, hitMessage);
+            }
+        }
+
+        public ParallelWriter GetParallelWriter() => this.writer;
+
+        
+
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+
+            var capacity = 10000;
+            this.keyEntities = new NativeList<Entity>(capacity, Allocator.Persistent);
+            this.messageHolder = new NativeMultiHashMap<Entity, HitMessageUnit>(capacity, Allocator.Persistent);
+        }
+
+        protected override void OnUpdate()
+        {
+
+            this.Dependency = new HitMessageApplyJob
+            {
+                MessageHolder = this.messageHolder,
+                KeyEntities = this.keyEntities.AsDeferredJobArray(),
+            }
+            .Schedule(this.keyEntities, 8, this.Dependency);
+
+
+            var ke = this.keyEntities;
+            var mh = this.messageHolder;
+            this.Job
+                .WithCode(
+                    () =>
+                    {
+                        ke.Clear();
+                        mh.Clear();
+                    }
+                )
+                .Schedule();
+        }
+    }
+
+    struct HitMessageApplyJob : IJobParallelForDefer
+    {
+        [ReadOnly]
+        public NativeMultiHashMap<Entity, HitMessageUnit> MessageHolder;
+
+        [ReadOnly]
+        public NativeArray<Entity> KeyEntities;
+
+        public void Execute(int index)
+        {
+
+        }
+    }
 }
