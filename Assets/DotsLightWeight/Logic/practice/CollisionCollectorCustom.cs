36a98de3846f2ee9b93e939e1814dce38dbcf7ad
abarabone <abara00@live.jp>
2021-05-27 22:35:34 +0900

    a

--- Assets/DotsLightWeight/Logic/practice/CollisionCollectorCustom.cs
+++ Assets/DotsLightWeight/Logic/practice/CollisionCollectorCustom.cs
@@ -5,6 +5,7 @@ using Unity.Entities;
 using Unity.Collections;
 using Unity.Physics;
 using System;
+using Unity.Mathematics;
 
 namespace DotsLite.Collision
 {
@@ -37,68 +38,125 @@ namespace DotsLite.Collision
             new AnyTargetedHitExcludeSelfCollector<T>(maxFraction, selfStateEntity, targets);
     }
 
-    
+    public struct HitResult
+    {
+        public HitType hitType;
+        public float3 posision;
+        public float3 normal;
+        public Entity hitEntity;
+        public Entity stateEntity;
+    }
+
     /// <summary>
     /// type 取得、state entity 取得、自己除外、すべて
     /// </summary>
-    //public struct AllTargetedHitExcludeSelfCollector<T> : ICollector<T>, IDisposable
+    public struct AllTargetedHitExcludeSelfCollector<> : ICollector<T>//, IDisposable
+        where T : struct, IQueryResult
+    {
+        public bool EarlyOutOnFirstHit => false;
+        public float MaxFraction { get; private set; }
+        public int NumHits { get; private set; }
+
+
+        Entity selfStateEntity;
+
+        ComponentDataFromEntity<Hit.TargetData> targets;
+
+        NativeList<HitResult> hitResults;
+
+
+        public AllTargetedHitExcludeSelfCollector
+            (float maxFraction, Entity selfStateEntity, ComponentDataFromEntity<Hit.TargetData> targets, NativeList<HitResult> outputs)
+        {
+            this.hitResults = outputs;
+
+            this.MaxFraction = maxFraction;
+            this.NumHits = 0;
+
+            this.selfStateEntity = selfStateEntity;
+            this.targets = targets;
+        }
+
+        public bool AddHit(T hit)
+        {
+            var res = new HitResult
+            {
+                hitEntity = hit.Entity,
+            };
+
+            if (this.targets.HasComponent(hit.Entity))
+            {
+                var t = this.targets[hit.Entity];
+                if (t.MainEntity == this.selfStateEntity) return false;
+
+                res.hitType = t.HitType;
+                res.stateEntity = t.MainEntity;
+            }
+            else
+            {
+                if (hit.Entity == this.selfStateEntity) return false;
+
+                res.hitType = HitType.none;
+                res.stateEntity = hit.Entity;
+            }
+
+            this.hitResults.Add(res);
+            this.NumHits = this.hitResults.Length;
+            return true;
+        }
+    }
+    //public struct AllTargetedHitExcludeSelfCollector<T> : ICollector<T>//, IDisposable
     //    where T : struct, IQueryResult
     //{
-    //    public bool EarlyOutOnFirstHit => false;//{ get; private set; }
+    //    public bool EarlyOutOnFirstHit => false;
     //    public float MaxFraction { get; private set; }
     //    public int NumHits { get; private set; }
 
-    //    public HitType OtherHitType { get; private set; }
-    //    public Entity OtherStateEntity { get; private set; }
 
     //    Entity selfStateEntity;
 
     //    ComponentDataFromEntity<Hit.TargetData> targets;
-        
 
-    //    public NativeList<T> HitResults { get; private set; }
-
-    //    public void Dispose() => this.HitResults.Dispose();
+    //    NativeList<HitResult> hitResults;
 
 
     //    public AllTargetedHitExcludeSelfCollector
-    //        (float maxFraction, Entity selfStateEntity, ComponentDataFromEntity<Hit.TargetData> targets, int capacity = 512)
+    //        (float maxFraction, Entity selfStateEntity, ComponentDataFromEntity<Hit.TargetData> targets, NativeList<HitResult> outputs)
     //    {
-    //        this.HitResults = new NativeList<T>(capacity, Allocator.Temp);
+    //        this.hitResults = outputs;
 
     //        this.MaxFraction = maxFraction;
     //        this.NumHits = 0;
 
-    //        //this.OtherStateEntity = Entity.Null;
-    //        //this.OtherHitType = HitType.none;
-
     //        this.selfStateEntity = selfStateEntity;
-            
     //        this.targets = targets;
     //    }
 
-    //    public bool AddHit( T hit )
+    //    public bool AddHit(T hit)
     //    {
+    //        var res = new HitResult
+    //        {
+    //            hitEntity = hit.Entity,
+    //        };
+
     //        if (this.targets.HasComponent(hit.Entity))
     //        {
     //            var t = this.targets[hit.Entity];
     //            if (t.MainEntity == this.selfStateEntity) return false;
 
-    //            //this.OtherHitType = t.HitType;
-    //            //this.OtherStateEntity = t.MainEntity;
-    //            this.HitResults.Add(hit);//
+    //            res.hitType = t.HitType;
+    //            res.stateEntity = t.MainEntity;
     //        }
     //        else
     //        {
     //            if (hit.Entity == this.selfStateEntity) return false;
 
-    //            //this.OtherHitType = HitType.none;
-    //            //this.OtherStateEntity = hit.Entity;
-    //            this.HitResults.Add(hit);//
+    //            res.hitType = HitType.none;
+    //            res.stateEntity = hit.Entity;
     //        }
 
-    //        //this.MaxFraction = hit.Fraction;
-    //        this.NumHits = this.HitResults.Length;
+    //        this.hitResults.Add(res);
+    //        this.NumHits = this.hitResults.Length;
     //        return true;
     //    }
     //}
