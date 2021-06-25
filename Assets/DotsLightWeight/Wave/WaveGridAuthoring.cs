a708035b13cf4e4990e7435fa0068520531d04c1
abarabone <abara00@live.jp>
2021-06-25 23:19:28 +0900

    a

--- Assets/DotsLightWeight/Wave/WaveGridAuthoring.cs
+++ Assets/DotsLightWeight/Wave/WaveGridAuthoring.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Linq;
 using UnityEngine;
+using Unity.Entities;
+using Unity.Collections;
+using Unity.Transforms;
+using Unity.Mathematics;
 
-public class WaveGridAuthoring : MonoBehaviour
+namespace DotsLite.WaveGrid.Aurthoring
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    using DotsLite.WaveGrid;
+    using DotsLite.Model;
+    using DotsLite.Draw;
+    using DotsLite.Model.Authoring;
+    using DotsLite.Draw.Authoring;
+    using DotsLite.Geometry;
 
-    // Update is called once per frame
-    void Update()
+    public class WaveGridAuthoring : MonoBehaviour, IConvertGameObjectToEntity
     {
-        
+
+        public float UnitDistance;
+        public int2 UnitLength;
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+        {
+            var gcs = conversionSystem;
+
+            dstManager.AddComponentData(entity, new WaveGridData
+            {
+                Units = new NativeArray<WaveUnit>(this.UnitLength.x * this.UnitLength.y, Allocator.Persistent),
+            });
+        }
+    }
+}
+namespace DotsLite.WaveGrid
+{
+    public class WaveGridData : IComponentData, IDisposable
+    {
+        public NativeArray<WaveUnit> Units;
+
+        public void Dispose() => this.Units.Dispose();
+    }
+    public struct WaveUnit
+    {
+        public float Next;
+        public float Curr;
+        public float Prev;
     }
 }
--- Assets/DotsLightWeight/Wave/WaveGridSystem.cs
+++ Assets/DotsLightWeight/Wave/WaveGridSystem.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Collections;
 
-public class WaveGridSystem : MonoBehaviour
+namespace DotsLite.WaveGrid
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    using DotsLite.Misc;
 
-    // Update is called once per frame
-    void Update()
+    [UpdateInGroup(typeof(SystemGroup.Simulation.Move.ObjectMoveSystemGroup))]
+    public class WaveGridSystem : SystemBase
     {
-        
+
+        WaveGridData grid;
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+
+            this.RequireSingletonForUpdate<WaveGridData>();
+
+            if (!this.HasSingleton<WaveGridData>()) return;
+            this.grid = this.GetSingleton<WaveGridData>();
+        }
+
+
+        protected override void OnUpdate()
+        {
+            var dt = this.Time.DeltaTime;
+            var dtrate = dt * TimeEx.PrevDeltaTimeRcp;
+            var sqdt = dt * dt;
+            var harfsqdt = 0.5f * sqdt;
+
+            this.Dependency = new WaveGridJob
+            {
+                Units = this.grid.Units,
+            }
+            .Schedule(this.grid.Units.Length, 1, this.Dependency);
+
+        }
+
+        struct WaveGridJob : IJobParallelFor
+        {
+            public NativeArray<WaveUnit> Units;
+
+            public void Execute(int index)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
     }
 }
