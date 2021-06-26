68ff19fbb6a1b080b4870cb6bed96d2c0dd805a6
abarabone <abara00@live.jp>
2021-06-26 13:07:27 +0900

    a

--- Assets/DotsLightWeight/Wave/WaveGridAuthoring.cs
+++ Assets/DotsLightWeight/Wave/WaveGridAuthoring.cs
@@ -21,8 +21,10 @@ namespace DotsLite.WaveGrid.Aurthoring
     {
 
         public float UnitDistance;
-        public int2 UnitLength;
+        public int2 UnitLengthInGrid;
+        public int2 NumGrids;
 
+        public Shader DrawShader;
 
 
         /// <summary>
@@ -31,26 +33,106 @@ namespace DotsLite.WaveGrid.Aurthoring
         public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
         {
             var gcs = conversionSystem;
+            var em = dstManager;
 
-            dstManager.AddComponentData(entity, new WaveGridData
+            var mesh = createGridMesh();
+            var mat = new Material(this.DrawShader);
+            gcs.CreateDrawModelEntityComponents(this.gameObject, mesh, mat, BoneType.T, 1, 
+                
+                );
+
+            setMasterComponent();
+            gcs.CreateDrawModelEntityComponents(this.gameObject, mesh, mat, BoneType.)
+            createAllGrids_(this.NumGrids, lodlevel: 0, model);
+
+            return;
+
+
+            void setMasterComponent()
             {
-                Units = new NativeArray<WaveUnit>(this.UnitLength.x * this.UnitLength.y, Allocator.Persistent),
-            });
+                var totalLength = this.UnitLengthInGrid.x * this.UnitLengthInGrid.y;
+                em.AddComponentData(entity, new WaveGridMasterData
+                {
+                    Units = new NativeArray<WaveUnit>(totalLength, Allocator.Persistent),
+                });
+            }
+
+            void createAllGrids_(int2 numgrids, int lodlevel, Entity model)
+            {
+                var q =
+                    from ix in Enumerable.Range(0, numgrids.x >> lodlevel)
+                    from iy in Enumerable.Range(0, numgrids.y >> lodlevel)
+                    select new int2(ix, iy);
+                q.ForEach(i => createGridEntity_(lodlevel, i, model));
+            }
+
+            void createGridEntity_(int lodlevel, int2 i, Entity model)
+            {
+                var ent = gcs.CreateAdditionalEntity(this);
+
+                gcs.DstEntityManager.SetName_(ent, $"wavegrid@{lodlevel}:{i.x}:{i.y}");
+
+
+                var types = new ComponentTypes(new ComponentType[]
+                {
+                        typeof(WaveGridData),
+                        typeof(DrawInstance.ModelLinkData),
+                        typeof(DrawInstance.TargetWorkData),
+                });
+                em.AddComponents(ent, types);
+
+                
+                em.SetComponentData(ent, new WaveGridData
+                {
+                    gridid = i,
+                    UnitScaleOnLod = this.UnitDistance * lodlevel,
+                });
+
+                em.SetComponentData(ent,
+                    new DrawInstance.ModelLinkData
+                    {
+                        DrawModelEntityCurrent = model,
+                    }
+                );
+                em.SetComponentData(ent,
+                    new DrawInstance.TargetWorkData
+                    {
+                        DrawInstanceId = -1,
+                    }
+                );
+            }
+
+            Mesh createGridMesh()
+            {
+
+            }
         }
+
     }
 }
 namespace DotsLite.WaveGrid
 {
-    public class WaveGridData : IComponentData, IDisposable
+    public class WaveGridMasterData : IComponentData, IDisposable
     {
         public NativeArray<WaveUnit> Units;
+        public int2 UnitLengthInGrid;
+        public int2 NumGrids;
 
-        public void Dispose() => this.Units.Dispose();
+        public void Dispose()
+        {
+            this.Units.Dispose();
+        }
     }
     public struct WaveUnit
     {
         public float Next;
         public float Curr;
         public float Prev;
+    }
+
+    public struct WaveGridData : IComponentData
+    {
+        public int2 gridid;
+        public float UnitScaleOnLod;
     }
 }
