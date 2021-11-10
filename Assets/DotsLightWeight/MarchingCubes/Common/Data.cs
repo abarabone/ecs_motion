3bb466cb8fc002ded583aff622ee6569706a23ca
abarabone <abara00@live.jp>
2021-11-10 23:18:58 +0900

    a

---
+++ Assets/DotsLightWeight/MarchingCubes/Common/Data.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using UnityEngine.Rendering;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.IO;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Burst;
+using Unity.Mathematics;
+using Unity.Collections.LowLevel.Unsafe;
+
+namespace DotsLite.MarchingCubes
+{
+
+    using DotsLite.Draw;
+    using DotsLite.Utilities;
+
+
+
+//common
+//work
+//init
+
+
+//type
+//dimension
+//link to grid
+//info
+//infowork
+//resource
+//prefab
+//rot
+//pos
+//init
+
+
+//common
+//  geometry data buffer resource
+//grid pool
+//  default
+//model
+//  shader
+//  cube id buffer texture resource
+//  dot content buffer resource
+//  cube instatnce buffer resource
+//area
+//  link to grid
+//  model entity
+//  pool entity
+//grid
+
+}
+
