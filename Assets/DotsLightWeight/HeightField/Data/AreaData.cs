796cab543a768d572a1bfab763933f67398c44d8
abarabone <abara00@live.jp>
2021-12-04 23:23:40 +0900

    a

---
+++ Assets/DotsLightWeight/HeightField/Data/AreaData.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using Unity.Entities;
+using Unity.Collections;
+using Unity.Transforms;
+using Unity.Mathematics;
+using System.Runtime.CompilerServices;
+
+namespace DotsLite.HeightField
+{
+    using DotsLite.Common.Extension;
+    using DotsLite.Misc;
+    using DotsLite.Common;
+    using DotsLite.Utilities;
+    using DotsLite.Mathematics;
+
+
+
+}
