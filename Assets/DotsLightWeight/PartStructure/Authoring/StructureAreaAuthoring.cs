0d39f601c026dad73373b2ad7d6fabbf77099b08
abarabone <abara00@live.jp>
2022-02-21 01:24:22 +0900

    a

--- Assets/DotsLightWeight/PartStructure/Authoring/StructureAreaAuthoring.cs
+++ Assets/DotsLightWeight/PartStructure/Authoring/StructureAreaAuthoring.cs
@@ -76,14 +76,10 @@ namespace DotsLite.Structure.Authoring
             //var bones = near.GetComponentsInChildren<StructureBone>();
 
             parts.DispatchPartId();
-            ////parts.BuildPartModelEntities(gcs);
 
-            ////this.QueryModel.CreateModelEntitiesWithDictionary(gcs);
-
-
-            ////this.WrapEnumerable<ModelGroupAuthoring.ModelAuthoringBase>()
-            ////    .Concat(parts)
-            ////    .BuildModelToDictionary(gcs);
+            this.WrapEnumerable<ModelGroupAuthoring.ModelAuthoringBase>()
+                .Concat(parts)
+                .BuildModelToDictionary(gcs);
 
             initBinderEntity_(gcs, top, posture);
             gcs.InitPostureEntity(posture);//, far.objectTop.transform);
